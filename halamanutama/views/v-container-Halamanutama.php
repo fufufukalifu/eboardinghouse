b5017ea72ce6a7a8cf5442227751cc7a01f3c2a9
fufufukalifu <opik@mail.unpas.aci>
2017-01-14 00:48:17 +0700

    commit

--- halamanutama/views/v-container-Halamanutama.php
+++ halamanutama/views/v-container-Halamanutama.php
@@ -1,16 +1,18 @@
-             
-<link rel="stylesheet" href="../assets/plugins/owl/css/owl.carousel.min.css">
+ 
+           
 
-<link rel="stylesheet" href="../assets/plugins/layerslider/css/layerslider.min.css">
+<link rel="stylesheet" href="<?=base_url('assets/plugins/owl/css/owl.carousel.min.css')?>">
+<link rel="stylesheet" href="<?=base_url('assets/plugins/layerslider/css/layerslider.min.css')?>">
+<script type="text/javascript" src="<?=base_url('assets/plugins/magnific/js/jquery.magnific-popup.min.js') ?>"></script><script type="text/javascript" src="<?=base_url('assets/plugins/shuffle/js/jquery.shuffle.min.js') ?>"></script>
 
 <section id="layerslider" style="width:100%;height:480px;">
  <div class="ls-slide" data-ls="transition2d:1; slidedelay:8000;">
   <!-- slide background -->
-  <img src="../assets/image/layerslider/bg3.png" class="ls-bg">
+  <img src="<?=base_url('assets/image/layerslider/bg3.png') ?>" class="ls-bg">
   <!--/ slide background -->
 
   <!-- Layer #1 -->
-  <img class="ls-l" style="top:90px;left:68%;" src="../assets/image/layerslider/layer/people2.png" data-ls="delayin:1000; easingin:easeOutElastic;">
+  <img class="ls-l" style="top:90px;left:68%;" src="<?=base_url('assets/image/layerslider/layer/people2.png') ?>" data-ls="delayin:1000; easingin:easeOutElastic;">
   <!--/ Layer #1 -->
 
   <!-- Layer #2 -->
@@ -41,51 +43,11 @@
   <!--/ Layer #5 -->
 
   <!-- Layer #6 -->
-  <img class="ls-l" style="top:320px;left:280px;" src="../assets/image/layerslider/layer/arrow.png" data-ls="delayin:3500; offsetxin:0; offsetyin:-30; easingin:easeOutElastic;">
+  <img class="ls-l" style="top:320px;left:280px;" src="<?=base_url('assets/image/layerslider/layer/arrow.png') ?>" data-ls="delayin:3500; offsetxin:0; offsetyin:-30; easingin:easeOutElastic;">
   <!--/ Layer #6 -->
  </div>
  <!-- Slide #1 -->
 
- <!-- Slide #2 -->
- <div class="ls-slide" data-ls="transition2d:1; slidedelay:8000;">
-  <!-- slide background -->
-  <img src="../assets/image/layerslider/bg2.png" class="ls-bg" alt="Slide background">
-  <!--/ slide background -->
-
-  <!-- Layer #1 -->
-  <h4 class="ls-l font-alt thin text-default text-right" style="top:120px;left:65%;width:550px;" data-ls="easingin:easeOutElastic; delayin:0;">
-   Fully Responsive &amp; Clean Design
-  </h4>
-  <!--/ Layer #1 -->
-
-  <!-- Layer #2 -->
-  <h1 class="ls-l font-alt text-right" style="top:150px;left:65%;width:550px;" data-ls="easingin:easeOutElastic; delayin:500;">
-   <span class="text-primary">Mobile</span> Devices Optimize
-  </h1>
-  <!--/ Layer #2 -->
-
-  <!-- Layer #3 -->
-  <p class="ls-l text-default text-right" style="top:210px;left:65%;width:550px;" data-ls="easingin:easeOutElastic; delayin:1000;">
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-   consequat.
-  </p>
-  <!--/ Layer #3 -->
-
-  <!-- Layer #5 -->
-  <p class="ls-l text-default text-right" style="top:290px;left:65%;width:550px;" data-ls="easingin:easeOutElastic; delayin:1500;">
-   <a href="javascript:void(0);" class="btn btn-primary">
-    Learn More <i class="ico-angle-right ml5"></i>
-   </a>
-  </p>
-  <!--/ Layer #5 -->
-
-  <!-- Layer #6 -->
-  <img class="ls-l" style="top:20px;left:250px;" src="../assets/image/layerslider/layer/iphone-5s.png" data-ls="delayin:2000; easingin:easeOutElastic;">
-  <!--/ Layer #6 -->
- </div>
- <!-- Slide #2 -->
 </section>
 
 <!-- START Call To Action Section -->
@@ -143,8 +105,8 @@
           </div>
          </div>
          <!--/ toolbar overlay -->
-         <?php  $img = "../assets/imagekosan/kosan".$i.".jpg" ?>
-         <img data-toggle="unveil" src="../assets/image/background/blog/placeholder.jpg" data-src="<?=$img ?>" alt="Photo" height="160px">
+         <?php  $img = base_url('assets/imagekosan/kosan'.$i.'.jpg')?>
+         <img data-toggle="unveil" src="<?=base_url('assets/image/background/blog/placeholder.jpg')?>" data-src="<?=$img ?>" alt="Photo" height="160px">
         </div>
         <!--/ media -->
        </header>
@@ -179,3 +141,9 @@
  <a href="#" class="totop animation" data-toggle="waypoints totop" data-showanim="bounceIn" data-hideanim="bounceOut" data-offset="50%"><i class="ico-angle-up"></i></a>
  <!--/ END To Top Scroller -->
 </section>
+      <script type="text/javascript" src="<?=base_url('assets/plugins/owl/js/owl.carousel.min.js') ?>"></script>
+
+      <script type="text/javascript" src="<?=base_url('assets/javascript/pages/frontend/portfolio.js') ?>"></script>
+        <script type="text/javascript" src="<?=base_url('assets/plugins/layerslider/js/layerslider.min.js')?>"></script>
+    
+      <script type="text/javascript" src="<?=base_url('assets/javascript/pages/frontend/home.js') ?>"></script>
